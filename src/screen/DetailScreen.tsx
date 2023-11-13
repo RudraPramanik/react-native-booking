ead4b6b57db4a75c29fc917aa556a7cde547c0e3
rudra pramanik <rudrahimangshoo2020@gmail.com>
2023-11-14 00:22:49 +0600

    rr

--- src/screen/DetailScreen.tsx
+++ src/screen/DetailScreen.tsx
@@ -1,18 +1,25 @@
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 const DetailScreen: React.FC<{ route: any }> = () => {
   // const { name, description, rating } = route.params;
 
   return (
     <View style={styles.container}>
+      <StatusBar
+        style="light" // Set the text color (dark/light)
+        backgroundColor="black" // Set the background color
+        translucent={true} // Make it translucent
+      />
       <View style={styles.section1}>
         <ImageBackground
-          source={require('../../assets/black-bg.png')} // replace with your image path
+          source={require('../../assets/v_half.png')} // replace with your image path
           style={styles.backgroundImage}
         >
           <View style={styles.container1}>
             {/* Your component content goes here */}
+
             <Text style={styles.text}>
               Hello, this is on top of the background image!
             </Text>
@@ -23,7 +30,9 @@ const DetailScreen: React.FC<{ route: any }> = () => {
       <View style={styles.section2}>
         {/* Content for the second section */}
       </View>
-      <View style={styles.section3}>{/* Content for the third section */}</View>
+      <View style={styles.section3}>
+        {/* Content for the second section */}
+      </View>
     </View>
   );
 };
@@ -32,14 +41,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column', // Default is 'column', which means vertical layout
+    backgroundColor: '#32075e',
   },
   section1: {
     flex: 1, // Takes 1/3 of the available vertical space
-    backgroundColor: 'lightblue', // Customize as needed
   },
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover', // or 'stretch' or 'contain'
+    resizeMode: 'cover', // or 'stretch' or 'contain' 'cover'
+    height: 300,
   },
   container1: {
     flex: 1,
